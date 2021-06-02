681afe6896d0759581ff62eb8a9d3116aa902e5b
Wellington <wellington_oliveira@live.com>
2021-06-02 11:36:53 -0400

    Adicionando bloqueio por horario

--- app.js
+++ app.js
@@ -3,25 +3,31 @@ const navigation = require('./browser/nav');
 const bufferProcess = require('./browser/buffer_process')
 const bufferInfo = require('./browser/voids_info')
 var nodemailer = require('nodemailer');
+var moment = require('moment'); // require
 
 
 async function app(){
     const browser = await puppeteer.launch(/*{headless: false}*/);
     const page = await browser.newPage();
+
+
     do {
+        if(moment().weekday()=="6" || moment().weekday()=="7"){
+            try {
+                await page.goto('http://mob2b-backend.cloudapp.net/User/LogOn?ReturnUrl=%2f');
+                await page.waitForTimeout(10000);
+                await navigation.login(page)
+                await bufferProcess.runBuffer_insert_or_update(await navigation.run(page),browser);
+                await page.waitForTimeout(10000);
+                await bufferInfo.run(page)
+            } catch (error) {
+                console.log("Erro geral")
+                console.log(error)
+            }    
+        }else{
+            console.log("Dia comum");
 
-        try {
-            await page.goto('http://mob2b-backend.cloudapp.net/User/LogOn?ReturnUrl=%2f');
-            await page.waitForTimeout(10000);
-            await navigation.login(page)
-            await bufferProcess.runBuffer_insert_or_update(await navigation.run(page),browser);
-            await page.waitForTimeout(10000);
-            await bufferInfo.run(page)
-        } catch (error) {
-            console.log("Erro geral")
-            console.log(error)
         }
-
     } while (true);
 
 }
--- teste.js
+++ teste.js
@@ -1,37 +1,10 @@
-const nodemailer = require("nodemailer");
+var moment = require('moment'); // require
+var CurrentDate = moment();
+console.log(moment().format("HH:mm"));
+console.log(moment().weekday());
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "email-ssl.com.br",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: "wellington.oliveira@amazoncopy.com.br", // generated ethereal user
-      pass: "We@201020460318", // generated ethereal password
-    },
-  });
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: 'wellington.oliveira@amazoncopy.com.br', // sender address
-    to: 'wellington.oliveira@amazoncopy.com.br', // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+if(moment().weekday()=="21"){
+  console.log("Terça")
+}else{
+  console.log("não é terça")
 }
-
-main().catch(console.error);
